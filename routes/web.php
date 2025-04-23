01baa6aad66221f8abc47ce00a3826d40cfc05e9
codingbad12 <nicholas200349@gmail.com>
2025-04-23 22:38:15 +0700

    SCRUM-15: Testing 1

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,31 @@
 <?php
-use App\Http\Controllers\JadwalController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileDokterController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\SupportController;
 
 Route::get('/', function () {
-    return redirect()->route('jadwal.index');
-})->name('home');
+    return view('welcome');
+});
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Admin routes
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
+});
 
 
-Route::get('/jadwal', [JadwalController::class, 'index'])->name('jadwal.index');
+// Login Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/profileD/{id}', [ProfileDokterController::class, 'profileD'])->name('profileD.index');
+// Support Routes
+Route::get('/support/contact', [SupportController::class, 'showContactForm'])->name('support.contact');
+Route::post('/support/contact', [SupportController::class, 'submitContact'])->name('support.submit');
+
+//Test
